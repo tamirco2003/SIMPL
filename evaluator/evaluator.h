3e30f717f0625985c2227e2ca7316300cfde3167
tamirco2003 <tamir.cohen835@gmail.com>
2020-10-18 08:48:28 +0300

    Added evaluator documentation.

--- evaluator/evaluator.h
+++ evaluator/evaluator.h
@@ -7,8 +7,14 @@
 // Exits the program when evaluator encounters an error.
 void runtimeError();
 
+// Takes in a pointer to a statement, evaluates it and each following statement
+// by calling evaluateStatement.
 void evaluate(Statement* firstStatement);
+
+// Takes in a statement pointer and evaluates, calls evaluateExpression.
 void evaluateStatement(Statement* statement);
+
+// Takes in a expression pointer and evaluates it, calls itself recursively.
 double evaluateExpression(Expression* expression);
 
 #endif
